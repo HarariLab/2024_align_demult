4ec6a88db608c08ec0a472850c801cf5a8b9f2c3
Iara Souza <iaradsouza1@gmail.com>
2025-02-05 16:35:05 -0500

    fix: fixed SCE object, added raw directory

--- workflow/scripts/create_cellranger_outs.R
+++ workflow/scripts/create_cellranger_outs.R
@@ -28,19 +28,35 @@ for (s in samples) {
 
     m_file <- all_matrix_input[grep(pools[s], all_matrix_input)]
     #print(m_file)
-    sparse_matrix <- read10xCounts(dirname(m_file))
-    sparse_matrix <- counts(sparse_matrix)
-    sparse_matrix <- sparse_matrix[, which(colnames(sparse_matrix) %in% samples_bc)]
+    sce <- read10xCounts(dirname(m_file))
+    sce_filtered <- sce[, which(colData(sce)$Barcode %in% samples_bc) ]
 
-    features <- rownames(sparse_matrix)
-    barcodes <- colnames(sparse_matrix)
+    sparse_matrix <- counts(sce)
+    sparse_matrix_filtered <- counts(sce_filtered)
 
-    sample_dir <- paste0("results/cellranger_gex_out/", s, "/outs/filtered_feature_bc_matrix/")
-    dir.create(sample_dir, recursive = T)
-    write10xCounts(path = sample_dir, 
+    features <- rowData(sce)$Symbol
+    barcodes <- colData(sce)$Barcode
+
+    features_filtered <- rowData(sce_filtered)$Symbol
+    barcodes_filtered <- colData(sce_filtered)$Barcode
+
+    # Create the filtered directory
+    sample_dir_filtered <- paste0("results/cellranger_gex_out/", s, "/outs/filtered_feature_bc_matrix/")
+    dir.create(sample_dir_filtered, recursive = T)
+    write10xCounts(path = sample_dir_filtered, 
+                   x = sparse_matrix_filtered, barcodes = barcodes_filtered, gene.id = features_filtered, 
+                   version = "3",
+                   overwrite = T)
+
+    # Create the raw directory
+    sample_dir_raw <- paste0("results/cellranger_gex_out/", s, "/outs/raw_feature_bc_matrix/")
+    dir.create(sample_dir_raw, recursive = T)
+    write10xCounts(path = sample_dir_raw, 
                    x = sparse_matrix, barcodes = barcodes, gene.id = features, 
                    version = "3",
                    overwrite = T)
+
+    
 
 }
 
